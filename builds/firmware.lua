42bf215eafa3e93c722534606a9af8448148be47
mateusmoutinho <mateusmoutinho01@gmail.com>
2026-02-08 14:24:00 -0300

    att

---
+++ builds/firmware.lua
@@ -0,0 +1,18 @@
+function firmware_build()
+
+    local compiler = "gcc"
+    if LAUNGUAGE == "cpp" then
+        compiler = "g++"
+    end
+
+    os.execute(compiler .. " -shared -o " .. "release/" .. PROJECT_NAME .. ".so" .."release/"..PROJECT_NAME..".c" )
+end
+
+darwin.add_recipe({
+    name = "firmware",
+    description = "Make a single so to be used combined with CWebStudioFirmware",
+    outs = {"release/" .. PROJECT_NAME .. ".so"},
+    inputs = {"main.c", "app.c", "dependencies","builds"},
+    requires={"amalgamation"}
+    callback = firmware_build
+})
