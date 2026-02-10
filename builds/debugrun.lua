56a96bc88b352b70d57a244da3c4e89e255104ec
mateusmoutinho <mateusmoutinho01@gmail.com>
2026-02-10 06:25:09 -0300

    att

--- builds/debugrun.lua
+++ builds/debugrun.lua
@@ -8,7 +8,7 @@ function debug_run()
         local new_hash = changes_hasher.get_value()
         if old_hash ~= new_hash then
             print("Building debug.so")
-            os.execute("gcc -shared -fPIC main.c -o debug.so")
+            os.execute(COMPILER.." -shared -fPIC main.c -o debug.so")
             if not darwin.dtw.isfile("debug.so") then
                 print("Failed to build debug.so")
             end
---
+++ builds/windows.lua
@@ -0,0 +1,30 @@
+function windowsi32_build()
+  
+
+    os.execute("mkdir -p release")
+
+    local image = darwin.ship.create_machine("debian:latest")
+    image.provider = CONTANIZER
+    image.add_comptime_command("apt-get update")
+    image.add_comptime_command("apt-get -y install mingw-w64")
+    local compiler = "i686-w64-mingw32-gcc"
+    if LAUNGUAGE == "cpp" then
+        compiler = "i686-w64-mingw32-g++"
+    end
+
+    image.start({
+        volumes = {
+            { "././release", "/release" },
+        },
+        command = compiler..[[ --static /release/]]..PROJECT_NAME..[[  -o /release/]]..PROJECT_NAME..[[.exe -lws2_32]]
+    })
+end
+
+darwin.add_recipe({
+    name = "windows",
+    requires={"amalgamation"},
+    description = "Builds a static Windows 32-bit executable "..PROJECT_NAME..".exe using MinGW",
+    outs = { "release/"..PROJECT_NAME..".exe" },
+    inputs = { "builds","main.c","app.c" },
+    callback = windowsi32_build
+})
